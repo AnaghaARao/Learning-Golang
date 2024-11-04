5b7f8a79ed4d9701042755a66083609b02274d79
AnaghaARao <rao.anagha2002@gmail.com>
2024-11-04 17:47:01 +0530

    varibles

---
+++ variables.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	var a = "intial"
+	fmt.Println(a)
+
+	var b, c int = 1,2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "apple"  
+	fmt.Println(f)
+
+	// := syntax is shorthand for declaring and initializing a variable
+	// for var f string = "apple" in this case
+	// This syntax is only available inside functions
+}
