47cda3e174b175d439845c5b543727e1b76bfce0
AnaghaARao <rao.anagha2002@gmail.com>
2024-12-18 19:39:15 +0530

    structs in go-1

---
+++ Structs/structs_in_go.go
@@ -0,0 +1,11 @@
+// Complete the definition of the messageToSend struct. It needs two fields:
+
+// phoneNumber - an integer
+// message - a string.
+
+package main
+
+type messageToSend struct {
+	phoneNumber int
+	message string
+}
